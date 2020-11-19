7b5f03e70be3398fe1c589246907e6d221d4196c
Leo-Mensah <leo.b.mensah@gmail.com>
2020-11-19 16:11:42 +0000

    Update exercise5.py

--- exercise5.py
+++ exercise5.py
@@ -1,2 +1,12 @@
-fname = open("romeo.txt")
-print(fname.split())
+first = input("Enter the file name: ")
+fname = open(first)
+lst = list()
+for name in fname:
+    flist = name.split()
+    for i in flist:
+        if i in lst :
+            continue
+        else:
+            lst.append(i)
+lst.sort()
+print(lst)
